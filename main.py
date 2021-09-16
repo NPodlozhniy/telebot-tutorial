26247353d95c8aca84acf883bf0603bca70e67c6
NPodlozhniy <n.podlozhniy@zelf.co>
2021-09-16 18:28:14 +0300

    Second Bot with using Heroku

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import argparse
 import os
 import telebot
+from flask import Flask, request
 
+BASE_URL = 'https://telebottutorial.herokuapp.com/'
+TELEBOT_URL = 'telebot_webhook/'
 
 API_TOKEN = os.environ['TOKEN']
 bot = telebot.TeleBot(API_TOKEN)
 
+server = Flask(__name__)
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
@@ -18,4 +23,28 @@ def echo_message(message):
     bot.reply_to(message, message.text)
 
 
-bot.polling()
+# Server passes messages to the bot
+@server.route('/' + TELEBOT_URL + API_TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+        
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=BASE_URL + TELEBOT_URL + API_TOKEN)
+    return "!", 200
+
+
+# Choose between webhook and polling mode depending on the command line argument
+parser = argparse.ArgumentParser(description='Run the bot')
+parser.add_argument('--local', action='store_true', help='run the bot in a local host')
+args = parser.parse_args()
+
+if args.local:
+    bot.remove_webhook()
+    bot.polling()
+else:
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    webhook()
