5a89f54a4a178482a2a4da3f91e2b9bbe3a6ce35
NPodlozhniy <n.podlozhniy@zelf.co>
2021-09-21 23:29:58 +0300

    Modify time sleeping for get_name testing

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dataloader import stats
 
 import time
 def test():
-    time.sleep(1)
+    time.sleep(10)
     a = range(1, 10)
     return f"Hello, dear colleague! \n Statistics for yesterday: \n {[x for x in a]}"
 
