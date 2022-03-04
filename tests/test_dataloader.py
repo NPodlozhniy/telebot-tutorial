73d0fa2a2aeceb5a1ba42d0a8284d459f25c34f7
NPodlozhniy <n.podlozhniy@zelf.co>
2022-03-04 17:57:48 +0300

    Increase testing delay

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -5,10 +5,10 @@ from dataloader import stats, lifetime
 
 def test_stats():
     for button in ["Cards", "Transactions", "Verifications"]:
-        time.sleep(random.randrange(0, 150, 30))
+        time.sleep(random.randrange(0, 210, 30))
         assert bool(stats(button)) == True
 
 
 def test_lifetime():
-    time.sleep(random.randrange(0, 150, 30))
+    time.sleep(random.randrange(0, 210, 30))
     assert bool(lifetime()) == True
