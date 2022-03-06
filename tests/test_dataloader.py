7bd5de7110045373158f854f63408e6095598b07
NPodlozhniy <n.podlozhniy@zelf.co>
2022-03-06 12:19:11 +0300

    Hot fix temp test

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -7,7 +7,7 @@ def test_stats():
     for button in ["Cards", "Transactions", "Verifications"]:
         # time.sleep(random.randrange(0, 210, 30))
         # assert bool(stats(button)) == True
-        assert True = True
+        assert True == True
 
 
 def test_lifetime():
